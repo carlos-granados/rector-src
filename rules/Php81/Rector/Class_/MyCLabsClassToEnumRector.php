82d19b402b6e15f0d8d461b1c0db7ab70f5302fa
Carlos Granados <barel.barelon@gmail.com>
2026-01-28 08:53:15 +0000

    MyCLabsClassToEnumRector: skip classes with properties or constructor
    - Added check to skip classes with properties (enums cannot have properties)
    - Added check to skip classes with __construct method (enums cannot have constructors)
    - Added test fixtures for both skip cases

--- rules/Php81/Rector/Class_/MyCLabsClassToEnumRector.php
+++ rules/Php81/Rector/Class_/MyCLabsClassToEnumRector.php
@@ -9,6 +9,7 @@ use PhpParser\Node\Stmt\Class_;
 use PHPStan\Type\ObjectType;
 use Rector\Php81\NodeFactory\EnumFactory;
 use Rector\Rector\AbstractRector;
+use Rector\ValueObject\MethodName;
 use Rector\ValueObject\PhpVersionFeature;
 use Rector\VersionBonding\Contract\MinPhpVersionInterface;
 use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
@@ -64,6 +65,14 @@ CODE_SAMPLE
     public function refactor(Node $node): ?Node
     {
         if (! $this->isObjectType($node, new ObjectType('MyCLabs\Enum\Enum'))) {
+            return null;
+        }
+
+        if ($node->getProperties() !== []) {
+            return null;
+        }
+
+        if ($node->getMethod(MethodName::CONSTRUCT) !== null) {
             return null;
         }
 
