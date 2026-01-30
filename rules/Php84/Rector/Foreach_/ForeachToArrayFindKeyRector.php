91e193726c75e30424cb0f9c95f9c076296107b2
Carlos Granados <barel.barelon@gmail.com>
2026-01-30 07:46:13 +0000

    Fix ForeachToArrayFindKeyRector: skip if statements with else/elseif branches

--- rules/Php84/Rector/Foreach_/ForeachToArrayFindKeyRector.php
+++ rules/Php84/Rector/Foreach_/ForeachToArrayFindKeyRector.php
@@ -182,6 +182,11 @@ CODE_SAMPLE
             return false;
         }
 
+        // skip if has else or elseif branches
+        if ($firstStmt->elseifs !== [] || $firstStmt->else !== null) {
+            return false;
+        }
+
         $assignmentStmt = $firstStmt->stmts[0];
         $breakStmt = $firstStmt->stmts[1];
 
